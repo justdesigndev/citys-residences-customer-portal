88368f0ff86a67d164bd8d30eb5d767a9df3a2e2
armagansenol <armagansnl@gmail.com>
2025-12-15 11:56:04 +0300

    fix: update z-index values in DialogOverlay and DialogContent for improved stacking context

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -27,7 +27,7 @@ function DialogOverlay({ className, ...props }: React.ComponentProps<typeof Dial
     <DialogPrimitive.Overlay
       data-slot='dialog-overlay'
       className={cn(
-        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-150 bg-black/80",
         className
       )}
       {...props}
@@ -50,7 +50,7 @@ function DialogContent({
         data-slot='dialog-content'
         className={cn(
           "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 duration-200",
-          "fixed top-[50%] left-[50%] z-50 translate-x-[-50%] translate-y-[-50%]",
+          "fixed top-[50%] left-[50%] z-150 translate-x-[-50%] translate-y-[-50%]",
           className
         )}
         {...props}
